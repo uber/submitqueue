075b955f9bd01b36363df4b8295b355df5061b70
Manjari Akella <asmanjari25@gmail.com>
2026-02-24 12:22:21 -0800

    feat(entities) Adds build entity and sql schema (#48)
    ## Summary
    feat(entities) Adds build entity and sql schema
    
    ## Why?
    <!-- Why is this change necessary? What is the motivation or
    justification? -->
    This sets up build entity and its corresponding sql schema. This
    establishes a concrete entity for builds that will be used across the
    system.
    
    ## What?
    * Add a `Build` entity
    * Add `build` mysql schema
    * Note: `BuildStore` in upcoming PR
    
    ## Test Plan
    
    
    ## Issues

---
+++ extension/storage/mysql/schema/build.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS build (
+    id VARCHAR(255) NOT NULL,
+    batch_id VARCHAR(255) NOT NULL,
+    speculation_path JSON NOT NULL,
+    score FLOAT NOT NULL,
+    status VARCHAR(64) NOT NULL,
+    PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
