7d6ea78a5eee49c152a2a3a558c7e3ad61e717d6
manjari <manjari@uber.com>
2026-02-24 00:09:13 +0000

    feat(entities) Adds batch dependent entity and sql schema

---
+++ extension/storage/mysql/schema/batch_dependent.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS batch_dependent (
+    batch_id VARCHAR(255) NOT NULL,
+    dependents JSON NOT NULL,
+    PRIMARY KEY (batch_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
