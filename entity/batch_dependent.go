1543bfd924571dc0d3687f23400b04819d70e74d
manjari <manjari@uber.com>
2026-02-24 00:09:13 +0000

    feat(entities) Adds batch dependent entity and sql schema

---
+++ entity/batch_dependent.go
@@ -0,0 +1,20 @@
+package entity
+
+// BatchDependent represents the downstream batches that depend on a given batch.
+// The object is immutable after creation.
+type BatchDependent struct {
+	// BatchID is the globally unique identifier representing a batch.
+	BatchID string
+	// Dependents is a list of batch IDs that are dependents for this
+	// batch.
+	//
+	// For e.g - Consider batches - queueA/batch/1, queueA/batch/2, queueA/batch/3
+	// such that - queueA/batch/2 and queueA/batch/3 depend on queueA/batch/1
+	//
+	// In this case, the Dependents field for -
+	// - queueA/batch/1 will be [queueA/batch/2, queueA/batch/3]
+	// - queueA/batch/2 will be empty
+	// - queueA/batch/3 will be empty
+	//
+	Dependents []string
+}
