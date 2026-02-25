8a1517eedf9af361a4283d0f3b6b34bb01820c42
kevin.new <kevin.new@uber.com>
2026-02-25 02:37:44 +0000

    remove the sentinel error

--- extension/mergechecker/mergechecker.go
+++ extension/mergechecker/mergechecker.go
@@ -2,19 +2,14 @@ package mergechecker
 
 import (
 	"context"
-	"errors"
 
 	"github.com/uber/submitqueue/entity"
 )
-
-// ErrUnmergeable is returned by Check when the request's changes are not mergeable.
-var ErrUnmergeable = errors.New("request is not mergeable")
 
 // MergeChecker predicts whether a request's changes can merge cleanly.
 type MergeChecker interface {
 	// Check is a fail-fast validation that optimistically assesses the
 	// mergeability of the request. A positive result does not guarantee
 	// that the changes will apply cleanly at merge finalization time.
-	// Returns ErrUnmergeable if the changes are not mergeable.
 	Check(ctx context.Context, request entity.Request) (bool, error)
 }
