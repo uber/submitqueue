0206276d4e6557ab2613c590ba2d64b8df776d53
manjari <manjari@uber.com>
2026-03-17 20:22:04 +0000

    pk is on request_id

--- extension/storage/mysql/schema/request_batch.sql
+++ extension/storage/mysql/schema/request_batch.sql
@@ -2,5 +2,5 @@ CREATE TABLE IF NOT EXISTS request_batch (
     request_id VARCHAR(255) NOT NULL,
     batch_id VARCHAR(255) NOT NULL,
     version INT NOT NULL,
-    PRIMARY KEY (request_id,batch_id)
+    PRIMARY KEY (request_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
