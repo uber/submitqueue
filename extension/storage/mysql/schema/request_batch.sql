2ab5f80b2e4f5af1e66945e1581f0342e8122dad
manjari <manjari@uber.com>
2026-03-17 20:18:30 +0000

    feat(schema) Add schema for request_batch; a reverse look up for request to batch

---
+++ extension/storage/mysql/schema/request_batch.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS request_batch (
+    request_id VARCHAR(255) NOT NULL,
+    batch_id VARCHAR(255) NOT NULL,
+    version INT NOT NULL,
+    PRIMARY KEY (request_id,batch_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
